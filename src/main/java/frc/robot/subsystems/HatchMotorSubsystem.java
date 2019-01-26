85f644b9a5c156c89bf396c0d8d8c9378aa9d0f9
Shawn Gwin <shawn@gwins.net>
2019-01-26 10:13:52 -0500

    Initial project with working encoder

---
+++ src/main/java/frc/robot/subsystems/HatchMotorSubsystem.java
@@ -0,0 +1,55 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+// import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.Encoder;
+import frc.robot.RobotMap;
+
+/**
+ * Add your docs here.
+ */
+public class HatchMotorSubsystem extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  Spark hatchMotorController;
+  Encoder hatchMotorEncoder;
+
+
+  public HatchMotorSubsystem(){
+    hatchMotorController = new Spark(RobotMap.hatchMotor);
+    hatchMotorEncoder = new Encoder(RobotMap.armEncoderA, RobotMap.armEncoderB);
+  }
+
+  public void turnClockwise() {
+    hatchMotorEncoder.reset();
+    while(hatchMotorEncoder.get() > -250){
+    hatchMotorController.set(-0.5);
+  }
+  hatchMotorController.set(0);
+    //Check the current value of the encoder to see if it is 180 degrees from the start value
+    //If it is then stop the motor. 
+  }
+
+  public void turnCounterclockwise(){
+    hatchMotorEncoder.reset();
+    while(hatchMotorEncoder.get() < 250){
+    hatchMotorController.set(0.5);
+  }
+  hatchMotorController.set(0);  
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
